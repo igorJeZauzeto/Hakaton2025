d9ac22bd9f414094fe52e24cb4374c43db34616c
Igor Vujosevic <hocudaigram9@gmail.com>
2025-09-13 15:29:12 +0200

    DDL for db

---
+++ db/Hakaton2025ddl.sql
@@ -0,0 +1,23 @@
+create database hakaton2025;
+CREATE TABLE Account (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+CREATE TABLE Drug(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    ATC VARCHAR(255),
+    INN VARCHAR(255),
+    maxPrice FLOAT,
+    available BOOLEAN,
+    description TEXT
+);
+CREATE TABLE Treatment(
+	idAcc INT,
+    idDrug INT,
+    description VARCHAR(255),
+    FOREIGN KEY (idAcc) REFERENCES Account(id),
+    FOREIGN KEY (idDrug) REFERENCES Drug(id)
+);
